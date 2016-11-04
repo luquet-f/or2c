74e629ea9775648fbdee735b6b06199ca300a029
Florent <florent.luquet@univ-nantes.fr>
2016-11-04 14:51:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Exemple de page Openlayers</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ol3/3.19.0/ol.css">
+        <link rel="stylesheet" href="static/css/style.css">
+    </head>
+
+    <body>
+        <div id="map" class="map"></div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ol3/3.19.0/ol-debug.js"></script>
+        <script src="static/js/app.js"></script>
+    </body>
+
+</html>
